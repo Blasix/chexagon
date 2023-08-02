0ab7df55e8ef0c89abf950de159f8c408e0efbca
Bas <BlasixCorp@gmail.com>
2023-08-02 12:53:17 +0200

    added the right colours to the board

--- lib/game_board.dart
+++ lib/game_board.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:hexagon/hexagon.dart';
 
@@ -17,11 +19,48 @@ class _GameBoardState extends State<GameBoard> {
         depth: 5,
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        buildTile: (coordinates) => HexagonWidgetBuilder(
-          padding: 2.0,
-          cornerRadius: 8.0,
-          child: Text('${coordinates.q}, ${coordinates.r}'),
-        ),
+        buildTile: (coordinates) {
+          // set the color of the tile based on the coordinates
+          Color? color;
+          if (coordinates.q % 3 == 0) {
+            if (coordinates.r % 3 == 0) {
+              color = Colors.grey[600];
+            } else if (coordinates.r % 3 == 1) {
+              color = Colors.grey[800];
+            } else {
+              color = Colors.grey[300];
+            }
+          } else if (coordinates.q % 3 == 1) {
+            if (coordinates.r % 3 == 0) {
+              color = Colors.grey[300];
+            } else if (coordinates.r % 3 == 1) {
+              color = Colors.grey[600];
+            } else {
+              color = Colors.grey[800];
+            }
+          } else {
+            if (coordinates.r % 3 == 0) {
+              color = Colors.grey[800];
+            } else if (coordinates.r % 3 == 1) {
+              color = Colors.grey[300];
+            } else {
+              color = Colors.grey[600];
+            }
+          }
+          // return a widget for the tile
+          return HexagonWidgetBuilder(
+            color: color,
+            padding: 2.0,
+            cornerRadius: 8.0,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('${coordinates.x}, ${coordinates.y}, ${coordinates.z}'),
+                Text('${coordinates.q}, ${coordinates.r}'),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
