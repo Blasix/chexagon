982d5a9f863f203ada4b2377d7057609813d0f79
Bas <BlasixCorp@gmail.com>
2023-08-02 14:38:44 +0200

    placed all pieces

--- lib/game_board.dart
+++ lib/game_board.dart
@@ -25,19 +25,106 @@ class _GameBoardState extends State<GameBoard> {
 
   // initialize board
   void _initBoard() {
-    List<List<ChessPiece?>> newBoard = List.generate(15, (i) {
-      if (i <= 5) {
-        return List.filled(i + 1, null);
-      } else if (i >= 10) {
-        return List.filled(15 - i, null);
-      } else if (i % 2 == 0) {
-        return List.filled(5, null);
-      } else {
-        return List.filled(6, null);
-      }
-    });
+    List<List<ChessPiece?>> newBoard =
+        List.generate(11, (_) => List.filled(11, null));
+
+    // place pawns
+    ChessPiece bPawn = ChessPiece(
+      type: ChessPieceType.pawn,
+      isWhite: false,
+      imagePath: 'images/pawn.png',
+    );
+    ChessPiece wPawn = ChessPiece(
+      type: ChessPieceType.pawn,
+      isWhite: true,
+      imagePath: 'images/pawn.png',
+    );
+    for (int i = 1; i <= 5; i++) {
+      newBoard[i][4] = bPawn;
+    }
+    for (int i = 1; i <= 4; i++) {
+      newBoard[5 + i][4 - i] = bPawn;
+    }
+    for (int i = 1; i <= 5; i++) {
+      newBoard[4 + i][6] = wPawn;
+    }
+    for (int i = 1; i <= 4; i++) {
+      newBoard[i][11 - i] = wPawn;
+    }
+
+    // place rooks
+    ChessPiece bRook = ChessPiece(
+      type: ChessPieceType.rook,
+      isWhite: false,
+      imagePath: 'images/rook.png',
+    );
+    ChessPiece wRook = ChessPiece(
+      type: ChessPieceType.rook,
+      isWhite: true,
+      imagePath: 'images/rook.png',
+    );
+    newBoard[2][3] = bRook;
+    newBoard[8][0] = bRook;
+    newBoard[2][10] = wRook;
+    newBoard[8][7] = wRook;
+
+    // place knights
+    ChessPiece bKnight = ChessPiece(
+      type: ChessPieceType.knight,
+      isWhite: false,
+      imagePath: 'images/knight.png',
+    );
+    ChessPiece wKnight = ChessPiece(
+      type: ChessPieceType.knight,
+      isWhite: true,
+      imagePath: 'images/knight.png',
+    );
+    newBoard[3][2] = bKnight;
+    newBoard[7][0] = bKnight;
+    newBoard[3][10] = wKnight;
+    newBoard[7][8] = wKnight;
+
+    // place bishops
+    ChessPiece bBishop = ChessPiece(
+      type: ChessPieceType.bishop,
+      isWhite: false,
+      imagePath: 'images/bishop.png',
+    );
+    ChessPiece wBishop = ChessPiece(
+      type: ChessPieceType.bishop,
+      isWhite: true,
+      imagePath: 'images/bishop.png',
+    );
+    for (int i = 0; i <= 2; i++) {
+      newBoard[5][i] = bBishop;
+      newBoard[5][10 - i] = wBishop;
+    }
+
+    // place queen
+    newBoard[4][1] = ChessPiece(
+      type: ChessPieceType.queen,
+      isWhite: false,
+      imagePath: 'images/queen.png',
+    );
+    newBoard[4][10] = ChessPiece(
+      type: ChessPieceType.queen,
+      isWhite: true,
+      imagePath: 'images/queen.png',
+    );
+
+    // place king
+    newBoard[6][0] = ChessPiece(
+      type: ChessPieceType.king,
+      isWhite: false,
+      imagePath: 'images/king.png',
+    );
+    newBoard[6][9] = ChessPiece(
+      type: ChessPieceType.king,
+      isWhite: true,
+      imagePath: 'images/king.png',
+    );
+
     board = newBoard;
-    // TODO: initialize the board with chess pieces
   }
 
   // declare a variable to hold the piece
@@ -51,14 +138,8 @@ class _GameBoardState extends State<GameBoard> {
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
         buildTile: (coordinates) {
-          //create a piece
-          piece = ChessPiece(
-            type: ChessPieceType.pawn,
-            isWhite: Random().nextBool(),
-            imagePath: 'images/pawn.png',
-          );
-
           // TODO: convert the 2-dimensional list index to pieces on the board
+          piece = board[5 + coordinates.q][5 + coordinates.r];
 
           // return a widget for the tile
           return HexagonWidgetBuilder(
