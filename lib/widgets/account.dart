55e47e512188c97109500c95eb1fb296428e4695
BlasixMac <blasixcorp@gmail.com>
2023-09-21 17:51:47 +0200

    added some dependencies for pfp selection added a license

--- lib/widgets/account.dart
+++ lib/widgets/account.dart
@@ -38,150 +38,169 @@ void showAccountDialog(BuildContext context, UserModel user) {
     return;
   }
   showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      height: 60,
-                      width: 60,
-                      child: InkWell(
-                        onTap: () {
-                          print('avatar');
-                        },
-                        borderRadius: BorderRadius.circular(50),
-                        child: Stack(
-                          children: [
-                            Align(
-                              alignment: Alignment.center,
-                              child: CircleAvatar(
-                                backgroundColor: Colors.transparent,
-                                backgroundImage: (user.pfpUrl != '')
-                                    ? null
-                                    : const AssetImage(
-                                        'images/pfp_placeholder.jpg'),
-                                foregroundImage: (user.pfpUrl == '')
-                                    ? null
-                                    : NetworkImage(user.pfpUrl),
-                                radius: 30,
-                              ),
-                            ),
-                            Align(
-                              alignment: Alignment.bottomRight,
-                              child: Container(
-                                padding: const EdgeInsets.all(4),
-                                decoration: BoxDecoration(
-                                  color: const Color(0xFFe3e3e3),
-                                  border: Border.all(color: cardColor!),
-                                  borderRadius: BorderRadius.circular(50),
-                                ),
-                                child: Icon(
-                                  FontAwesomeIcons.pen,
-                                  color: bgColor,
-                                  size: 14,
-                                ),
-                              ),
-                            ),
-                          ],
+    context: context,
+    builder: (context) => AlertDialog(
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(
+                height: 60,
+                width: 60,
+                child: InkWell(
+                  onTap: () {
+                    print('avatar');
+                  },
+                  borderRadius: BorderRadius.circular(50),
+                  child: Stack(
+                    children: [
+                      Align(
+                        alignment: Alignment.center,
+                        child: CircleAvatar(
+                          backgroundColor: Colors.transparent,
+                          backgroundImage: (user.pfpUrl != '')
+                              ? null
+                              : const AssetImage('images/pfp_placeholder.jpg'),
+                          foregroundImage: (user.pfpUrl == '')
+                              ? null
+                              : NetworkImage(user.pfpUrl),
+                          radius: 30,
                         ),
                       ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        const Text('Joined'),
-                        Text(
-                          calculateAccountAge(user.createdAt),
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: Container(
+                          padding: const EdgeInsets.all(4),
+                          decoration: BoxDecoration(
+                            color: const Color(0xFFe3e3e3),
+                            border: Border.all(color: cardColor!),
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          child: Icon(
+                            FontAwesomeIcons.pen,
+                            color: bgColor,
+                            size: 14,
                           ),
                         ),
-                      ],
-                    )
-                  ],
-                ),
-                const SizedBox(height: 10),
-                InkWell(
-                  borderRadius: BorderRadius.circular(5),
-                  onTap: () {
-                    print('username');
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        user.username,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 22,
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(4),
-                        decoration: BoxDecoration(
-                          border: Border.all(color: cardColor!),
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                        child: Icon(
-                          FontAwesomeIcons.pen,
-                          color: bgColor,
-                          size: 18,
-                        ),
                       ),
                     ],
                   ),
                 ),
-                InkWell(
-                  borderRadius: BorderRadius.circular(5),
-                  onTap: () {
-                    print('mail');
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        user.email,
-                        style: const TextStyle(
-                          fontSize: 22,
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(4),
-                        decoration: BoxDecoration(
-                          border: Border.all(color: cardColor!),
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                        child: Icon(
-                          FontAwesomeIcons.pen,
-                          color: bgColor,
-                          size: 18,
-                        ),
-                      ),
-                    ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  const Text('Joined'),
+                  Text(
+                    calculateAccountAge(user.createdAt),
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+          const SizedBox(height: 10),
+          InkWell(
+            borderRadius: BorderRadius.circular(5),
+            onTap: () {
+              print('username');
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  user.username,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22,
                   ),
                 ),
-                const SizedBox(height: 10),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                      context.go('/login');
-                      FirebaseAuth.instance.signOut();
-                    },
-                    child: const Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(FontAwesomeIcons.arrowRightFromBracket,
-                            color: Colors.red, size: 20),
-                        SizedBox(width: 6),
-                        Text('Logout'),
-                      ],
-                    )),
+                Container(
+                  padding: const EdgeInsets.all(4),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: cardColor!),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Icon(
+                    FontAwesomeIcons.pen,
+                    color: bgColor,
+                    size: 18,
+                  ),
+                ),
               ],
             ),
-          ));
+          ),
+          InkWell(
+            borderRadius: BorderRadius.circular(5),
+            onTap: () {
+              print('mail');
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  user.email,
+                  style: const TextStyle(
+                    fontSize: 22,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(4),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: cardColor!),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Icon(
+                    FontAwesomeIcons.pen,
+                    color: bgColor,
+                    size: 18,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: () async {
+              Navigator.pop(context);
+              await FirebaseAuth.instance.signOut();
+              if (context.mounted) context.go('/login');
+            },
+            child: const Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(FontAwesomeIcons.arrowRightFromBracket,
+                    color: Colors.red, size: 20),
+                SizedBox(width: 6),
+                Text('Logout'),
+              ],
+            ),
+          ),
+          const SizedBox(height: 6),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+              // TODO delete account
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.red,
+            ),
+            child: const Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(FontAwesomeIcons.trash, color: Colors.white, size: 20),
+                SizedBox(width: 6),
+                Text('Delete'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
 }
